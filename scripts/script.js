d2b6c9fb61304b447fe3f1955af1f8cca95b6636
Yasser Salama <yasser.salama@gmail.com>
2023-07-10 15:37:41 +0300

    Randomize Colors
    When hover a DIV the background color of this DIV will be randomized.

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,7 @@ function createDrawingGrid(parentElementId, size) {
 }
 
 function sketchDivMouseOverHandler() {
-    this.style.background = "orange";
+    this.style.background = randomRgbColor();
 }
 
 function gridSizePrompt() {
@@ -46,4 +46,15 @@ function gridSizePrompt() {
     }
 
     createDrawingGrid("container",Number(size),)
+}
+
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return `rgb(${r},${g},${b})`;
 }
